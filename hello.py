2c68f294539504aa3423de3702358206d19f478e
bhavikup <Bhavikup@gmail.com>
2016-04-07 15:57:08 -0700

    added python hello

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+print "Content-type: text/html \n\n"
+print "<html><head><title>Hello Python Based CGI World</title></head>"
+print "<body style='background-color: purple'><h1>Hello World from Python in the CSE136 lecture!</h1>"
+print "</body></html>"
+
