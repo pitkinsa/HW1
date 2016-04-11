94a5b68abaa52fea294fbe5a24b507d91e93b8aa
Bal Krishna Oli <bkoli@ucsd.edu>
2016-04-10 22:10:55 -0700

    Arranged files

--- hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import cgitb
-cgitb.enable()
-print "Content-type: text/html \n\n"
-print "<html><head><title>Hello Python Based CGI World</title></head>"
-print "<body style='background-color: purple'><h1>Hello World from Python in the CSE136 lecture!</h1>"
-print "</body></html>"
